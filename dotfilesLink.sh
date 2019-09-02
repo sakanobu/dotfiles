2bdd6ba839b68e64ad595a1a3cb6f5b432dde6b1
Amagi109Souma <grapejuice100percent@gmail.com>
2019-09-02 18:33:11 +0900

    modify dotfilesLink.sh about .vim slink

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 # ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -snf ~/dotfiles/.vim ~/.vim
