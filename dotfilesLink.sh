6cfafca65dfb6fb10104b458a9ce1e7a48fdf8ff
Amagi109Souma <grapejuice100percent@gmail.com>
2019-09-02 20:12:09 +0900

    modify commentout

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,5 +3,5 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-# ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snf ~/dotfiles/.vim ~/.vim
