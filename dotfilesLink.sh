0230685e70c5307bdea683668fb9084dbf4b7f61
sakanobu <grapejuice100percent@gmail.com>
2025-10-27 02:46:54 +0900

    シンボリックリンクを探すコマンドのコメントの追加、および、その出力先のファイルの追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,7 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/dot_git/ignore ~/.config/git/ignore
+
+# 大雑把にシンボリックリンクを探す
+# sudo find ~ -type l -maxdepth 6 2>/dev/null | grep -vE "permitted|Containers|Cache|com\.|iCloud|apple|Google|Brave|jp\.naver|Homebrew|zoom|discord|Docker|docker|vim-lsp-settings|Rider\.app|rbenv|node|yarn|venv|veve|virtualenvs|dylib|SDL" > ~/dotfiles/find_symbolic_link.txt
